66c38e5bfa537dbab4c19151766e8fb030427520
Harish <harishkumarreddy502@gmail.com>
2023-12-15 22:09:50 +0530

    This is about arguments with varibles

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this is about aruguments passing running script 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello How are you"
+echo "$PERSON2: I am good What about you"
+echo "$PERSON1: I am also doing good , What else from your side "
+echo "$PERSON2: Okay fine then "
